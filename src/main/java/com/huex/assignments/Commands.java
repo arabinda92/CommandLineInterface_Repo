17259c1bf0781c4e4c1735629e922034d0fb4ff2
Arabinda kumar <arabinda.dhani@hashedin.com>
2020-11-15 16:31:00 +0530

    lates changes

--- src/main/java/com/huex/assignments/Commands.java
+++ src/main/java/com/huex/assignments/Commands.java
@@ -1,18 +1,8 @@
 package com.huex.assignments;
-
-import com.sun.net.httpserver.Headers;
-import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
-import org.springframework.http.HttpEntity;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-import picocli.CommandLine;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 
 /**
@@ -26,7 +16,7 @@ public class Commands {
                                                  String orderBy) {
     WebClient.Builder webclient = WebClient.builder();
     List<CSVModel> responseList = webclient.build().get()
-      .uri("http://localhost:8080/api/v1/Reports/viewedPages/?orderBy=views&count="+n)
+      .uri("http://localhost:8080/Reports/viewedPages/?orderBy="+orderBy+"&count="+n)
       .retrieve().bodyToMono(List.class).block();
       return responseList;
   }
@@ -37,20 +27,32 @@ public class Commands {
                                                  String orderBy) {
     WebClient.Builder webclient = WebClient.builder();
     List<CSVModel> responseList = webclient.build().get()
-      .uri("http://localhost:8080/api/v1/Reports/pages/?orderBy=timeSpent&count="+n)
+      .uri("http://localhost:8080/Reports/pages/?orderBy="+orderBy+"&count="+n)
       .retrieve().bodyToMono(List.class).block();
     return responseList;
   }
 
   @ShellMethod("Displays n most viewed pages with date filters as optional values")
   public List<CSVModel> displayActiveUsers(@ShellOption({"-N", "--n"}) Long n,
-                                                  @ShellOption({"-N", "--orderBy"})
+                                           @ShellOption({"-N", "--orderBy"})
                                                     String orderBy) {
     WebClient.Builder webclient = WebClient.builder();
     List<CSVModel> responseList = webclient.build().get()
-      .uri("http://localhost:8080/api/v1/Reports/pages/?orderBy=timeSpent&count="+n)
+      .uri("http://localhost:8080/Reports/users/?orderBy="+orderBy+"&count="+n)
       .retrieve().bodyToMono(List.class).block();
     return responseList;
   }
 
+  @ShellMethod("Displays periodic reports as per the date filters ")
+  public List<CSVModel> displayTimelyReports(@ShellOption({"-N", "--n"}) Long n,
+                                             @ShellOption({"-N", "--orderBy"}) String orderBy,
+                                             @ShellOption({"-N", "--fromDate"}) String fromDate,
+                                             @ShellOption({"-N", "--toDate"}) String toDate) {
+    WebClient.Builder webclient = WebClient.builder();
+    List<CSVModel> responseList = webclient.build().get()
+      .uri("http://localhost:8080/Reports/users/?orderBy="+orderBy+"&count="
+        +n+"&fromDate="+fromDate+"&toDate="+toDate)
+      .retrieve().bodyToMono(List.class).block();
+    return responseList;
+  }
 }
